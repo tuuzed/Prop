e1ff70396d122f94f8fe8b54464386d79f7798c2
tuuzed <liuyonghui.job@gmail.com>
2019-11-14 21:58:29 +0800

    更新prop-databinding模块

--- prop-databinding/src/main/java/com/tuuzed/androidx/prop/BindingAdapters.java
+++ prop-databinding/src/main/java/com/tuuzed/androidx/prop/BindingAdapters.java
@@ -4,7 +4,12 @@ package com.tuuzed.androidx.prop;
 import android.widget.TextView;
 
 import androidx.databinding.BindingAdapter;
+import androidx.databinding.BindingMethod;
+import androidx.databinding.BindingMethods;
 
+@BindingMethods(
+        @BindingMethod(type = TextView.class, attribute = "bind:prop", method = "bindProp")
+)
 public class BindingAdapters {
 
     @BindingAdapter("bind:prop")
